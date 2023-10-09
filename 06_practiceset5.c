1bd45815f717c36182fbf3f898caf7a5c9a0fe72
RISHAV KISHORE <rishavkishore732@gmail.com>
2023-10-09 22:37:41 +0530

    Create 06_practiceset5.c

---
+++ 06_practiceset5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void sumAndavg(int a,int b,int *sum,float *avg){
+    *sum=a+b;
+    *avg=(float)(*sum)/2;
+}
+int main(){
+    int i,j,sum;
+    float avg;
+    printf("Enter the value of i\n");
+    scanf("%d",&i);
+    printf("Enter the value of j\n");
+    scanf("%d",&j);
+    sumAndavg(i,j,&sum,&avg);
+    printf("The value of sum is %d\n",sum);
+    printf("The value of avg is %f\n",avg);
+    return 0;
+}
